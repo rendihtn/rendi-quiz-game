3d4491cebf9e8df092f3ce54170d3b9d796c6729
Rendi Hartono <97784537+rendihtn@users.noreply.github.com>
2022-01-17 11:56:37 +0700

    Add files via upload

---
+++ quiz game.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  6 18:52:19 2022
+
+@author: ASUS
+"""
+
+print("Hi! Ini adalah quiz game milik saya")
+
+main = input("Apakah kamu ingin bermain?:")
+
+if main != "Mau":
+    quit()
+    
+print("oke ayo kita mulai")
